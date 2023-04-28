23957c8170b9add2d633f815f5db06dce33fe3b7
Howie315 <nguyen.howie2010@gmail.com>
2023-04-27 17:30:20 -0700

    added the fetch function to work perfectly

--- index.js
+++ index.js
@@ -1,3 +1,49 @@
-$(document).ready(function () {
-	const url = "http://50.21.190.71/get tweets";
-});
+// const tweetsList = document.getElementById("posts");
+// const searchInput = document.getElementById("search-Input");
+// // function to fetch tweets from the server
+// async function getTweets() {
+//   try {
+//     const response = await fetch("http://50.21.190.71/get_tweets");
+//     const data = await response.json();
+//     return data.statuses;
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+// select the element where the tweets will be displayed
+function fetchAndUpdateTweets() {
+  fetch("http://50.21.190.71/get_tweets")
+    .then((response) => response.json())
+    .then((data) => {
+      const tweetsContainer = document.getElementById("tweets-container");
+      tweetsContainer.innerHTML = ""; // clear existing tweets
+      data.forEach((tweet) => {
+        const tweetHTML = `
+	<div class="posts">
+	  <img class="slogopic" src="${tweet.avatar}" alt ="./images/linguini.png" />
+	  <div class="words">
+		<p>
+		  <span class="name">${tweet.user_name}</span>
+		  <span class="dates">${tweet.user_created}</span>
+		</p>
+		<p>${tweet.text}</p>
+	  </div>
+	</div>
+  `;
+        tweetsContainer.innerHTML += tweetHTML;
+      });
+    })
+    .catch((error) => {
+      console.error("Error fetching tweets:", error);
+    });
+}
+
+// Call the function once to initially display tweets
+fetchAndUpdateTweets();
+
+// Call the function every 10 seconds to update tweets
+setInterval(fetchAndUpdateTweets, 10000);
+
+
+
+
