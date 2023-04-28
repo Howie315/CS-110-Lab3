3eba96169378cbc99d885af6f9cff0a3406a2419
Howie315 <nguyen.howie2010@gmail.com>
2023-04-27 18:42:08 -0700

    it's in chronological order now

--- index.js
+++ index.js
@@ -1,35 +1,25 @@
-// const tweetsList = document.getElementById("posts");
-// const searchInput = document.getElementById("search-Input");
-// // function to fetch tweets from the server
-// async function getTweets() {
-//   try {
-//     const response = await fetch("http://50.21.190.71/get_tweets");
-//     const data = await response.json();
-//     return data.statuses;
-//   } catch (error) {
-//     console.log(error);
-//   }
-// }
-// select the element where the tweets will be displayed
 function fetchAndUpdateTweets() {
   fetch("http://50.21.190.71/get_tweets")
     .then((response) => response.json())
     .then((data) => {
+      // Sort the tweets by chronological order
+      data.sort((a, b) => new Date(a.created_at) - new Date(b.created_at));
+
       const tweetsContainer = document.getElementById("tweets-container");
       tweetsContainer.innerHTML = ""; // clear existing tweets
       data.forEach((tweet) => {
         const tweetHTML = `
-	<div class="posts">
-	  <img class="slogopic" src="${tweet.avatar}" alt ="./images/linguini.png" />
-	  <div class="words">
-		<p>
-		  <span class="name">${tweet.user_name}</span>
-		  <span class="dates">${tweet.user_created}</span>
-		</p>
-		<p>${tweet.text}</p>
-	  </div>
-	</div>
-  `;
+			<div class="posts">
+			  <img class="slogopic" src="${tweet.avatar}" alt="./images/ratatouille" />
+			  <div class="words">
+				<p>
+				  <span class="name">${tweet.user_name}</span>
+				  <span class="dates">${tweet.user_created}</span>
+				</p>
+				<p>${tweet.text}</p>
+			  </div>
+			</div>
+		  `;
         tweetsContainer.innerHTML += tweetHTML;
       });
     })
@@ -43,11 +33,3 @@ fetchAndUpdateTweets();
 
 // Call the function every 10 seconds to update tweets
 setInterval(fetchAndUpdateTweets, 10000);
-
-
-
-
-
-
-
-
