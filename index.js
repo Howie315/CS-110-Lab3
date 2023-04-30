1e4c1edeca86a5fed9b29780feb7229593310448
boney171 <ttran456@ucr.edu>
2023-04-29 17:18:34 -0700

    Fix avatars of each pics, added a search function and add event listener to inputsearch

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 
+//Global array variable to store json data in array form
+var tweets;
+
 function fetchAndUpdateTweets() {
   if (!document.querySelector("#pauseTweets").checked) {
     fetch("http://50.21.190.71/get_tweets")
       .then((response) => response.json())
       .then((data) => {
+
         // Sort the tweets by chronological order
         data.sort((a, b) => new Date(a.created_at) - new Date(b.created_at));
         
@@ -12,24 +16,24 @@ function fetchAndUpdateTweets() {
         tweetsContainer.innerHTML = ""; // clear existing tweets
        
         //Convert json data to an array
-       const tweets = data;
+        tweets = data;
        //Filter out the duplicated elements
-       const tweetsWithoutDuplicates = tweets.filter((user_name, index, self) => {
-        return self.indexOf(user_name) === index;
-        
-      });
-      console.log(tweetsWithoutDuplicates);
-      //Output the first 5 elements in the array only
+       const tweetsWithoutDuplicates = tweets.filter((value, index, self) => 
+       self.findIndex(v => v.user_name === value.user_name) === index);
+
+      //console.log(tweetsWithoutDuplicates);
+
+      //Output the first 10 elements in the array only
       for(var i = 0; i < tweetsWithoutDuplicates.length; i++){
         const tweetHTML = `
 			<div class="posts">
-      <img class="slogopic" src="${tweetsWithoutDuplicates[i].avatar}" onerror="this.onerror=null; this.src='./images/ratatouille';" alt="Profile picture for user ${tweets[i].user_name}" />
+      <img class="slogopic" src="${tweetsWithoutDuplicates[i]?.avatar}" onerror="this.onerror=null; this.src='./images/ratatouille.jpg';" alt="Profile picture for user ${tweetsWithoutDuplicates[i]?.user_name}" />
 			  <div class="words">
 				<p>
 				  <span class="name">${tweetsWithoutDuplicates[i].user_name}</span>
 				  <span class="dates">${tweetsWithoutDuplicates[i].user_created}</span>
 				</p>
-				<p>${tweets[i].text}</p>
+				<p>${tweetsWithoutDuplicates[i].text}</p>
 			  </div>
 			</div>
 		  `;
@@ -43,12 +47,42 @@ function fetchAndUpdateTweets() {
   }
 }
 
+let searchWords = "";
+const myForm= document.getElementById('search-input');
+document.getElementById('search-input').addEventListener('input', function(e) {
+  let searchWords = e.target.value;// this will log the current value of the input field whenever it changes
+  let filtered = tweets.filter(tweet => 
+    tweet.text.includes(searchWords) || tweet.date === searchWords || tweet.user_name.includes(searchWords)
+);
+
+const tweetsContainer = document.getElementById("tweets-container");
+        tweetsContainer.innerHTML = ""; // clear existing tweets
+for(var i = 0; i < filtered.length; i++){
+  const tweetHTML = `
+<div class="posts">
+<img class="slogopic" src="${filtered[i]?.avatar}" onerror="this.onerror=null; this.src='./images/ratatouille.jpg';" alt="Profile picture for user ${filtered[i][i]?.user_name}" />
+  <div class="words">
+  <p>
+    <span class="name">${filtered[i].user_name}</span>
+    <span class="dates">${filtered[i].user_created}</span>
+  </p>
+  <p>${filtered[i].text}</p>
+  </div>
+</div>
+`;
+  tweetsContainer.innerHTML += tweetHTML;
+}
+});
+
+
+
+
 
 // Call the function once to initially display tweets
 fetchAndUpdateTweets();
 
 // Call the function every 10 seconds to update tweets
-setInterval(fetchAndUpdateTweets, 10000);
+setInterval(fetchAndUpdateTweets, 5000);
 
 
 
